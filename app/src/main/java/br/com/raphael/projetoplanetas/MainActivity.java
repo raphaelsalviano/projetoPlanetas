bddf3761f6f55b7798f199f6f6a673e837b8d420
raphaelsalviano <raphael.salviano@dce.ufpb.br>
2016-03-11 14:36:24 -0300

    commit

--- app/src/main/java/br/com/raphael/projetoplanetas/MainActivity.java
+++ app/src/main/java/br/com/raphael/projetoplanetas/MainActivity.java
@@ -1,10 +1,9 @@
 package br.com.raphael.projetoplanetas;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,7 +23,8 @@ public class MainActivity extends AppCompatActivity {
         (findViewById(R.id.button2)).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                Intent intent = new Intent(MainActivity.this, RecyclerViewActivity.class);
+                startActivity(intent);
             }
         });
 
